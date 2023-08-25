add8dbdf787edbf00447e8bfbe2e7e3ad3feefb0
Nathan Welliver <nwelliver.564@gmail.com>
2023-08-25 12:46:16 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  //Loop through the array using tow pointers
+  for (let i = 0; i < array.length - 1; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      //if the sum of the current pair equals the target, return true
+      if (array[i] + array[j] === target) {
+        return true;
+      }
+    }
+  }
+  //if no pair found, return false
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  The time complexity is 0(n^2), where n is the length of the array.
 */
 
 /* 
   Add your pseudocode here
+  1. loop through the array using two pointers
+  2. for each pair of elements, check if their sum equals the target
+  3. if found, return true
+  if no pair found, return false
 */
 
 /*
   Add written explanation of your solution here
+  This function uses a nested loop to iterate through all possible pairs of numbers in the array.
+  It checks if the sum of the current pair equals the targetsum. If such a pair is found,
+  it returns true. If no such pair is found, it returns false.
 */
 
 // You can run `node index.js` to view these console logs
